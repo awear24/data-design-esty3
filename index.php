b92aed4a3816f53b8e60dfd301f2ee2108fead82
Augustina Wear <awear@cnm.edu>
2017-07-15 14:24:09 -0600

    Updated Frustrations and Needs

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Persona</title>
+	</head>
+	<body>
+		<p><strong>Persona</strong></p>
+		<p><strong>Name</strong> Sally Seashell</p>
+
+		<p><strong>Age</strong> 27</p>
+
+		<p><strong>Profession</strong></p>
+		<p>Artist specializing in sculpting</p>
+
+		<p><strong>Technology</strong></p>
+
+		<p>Mac Book Air user, corei5 and i7 processors, Intel HD graphics 6000, Sally has I phone 6s.</p>
+		<p> Sallie pays wifi modem from Xfinity that she pays fifty dollars a month.</p>
+
+
+		<p><strong>Attitudes and Behaviors</strong></p>
+		<p>Sally is not intimidated by technology she only uses her Mac about one hour to two hours a day. She is raising 3 small children and is a caretaker</p>
+		<p>for her disabled veteran husband. Sally stress level is higher than most.</p>
+
+		<p><strong>Frustrations and Needs</strong></p>
+
+		<p>Sallie frustration is that she has three small chilren and due to her disable veteran health issues.</p>
+		<p> Sallie needs manage her time more effectively by being able to post quickly.</p>
+		<p>Etsy has solved the issues to sell her product and create a second income stream.</p>
+
+		<p>Sallie's goal is to provide a creative outlet for people who want to be fashionable while meeting thier fitness goals.  </p>
+	</body>
