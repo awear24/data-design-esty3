371c369378e61b6bc647d5fbf32efe5f323d9522
Augustina Wear <awear@cnm.edu>
2017-07-16 19:51:30 -0600

    Fixed bugs added list

--- index.php
+++ index.php
@@ -15,26 +15,29 @@
 
 		<p><strong>Technology:</strong></p>
 
-		<p>Mac Book Air user, corei5 and i7 processors, Intel HD graphics 6000, Sally has I phone 6s.</p>
-		<p> Sallie pays wifi modem from Xfinity that she pays fifty dollars a month.</p>
-
-
+		<ul>
+			<li>Mac Book Air user, core i5 and i7 processors, Intel HD graphics 6000, Sally has I phone 6s.</li>
+		   <li> Sallie pays wifi modem from Xfinity that she pays fifty dollars a month.</li>
+		</ul>
 		<p><strong>Attitudes and Behaviors:</strong></p>
-		<p>Sally is not intimidated by technology she only uses her Mac about one hour to two hours a day. She is raising 3 small children and is a caretaker</p>
-		<p>for her disabled veteran husband. Sally stress level is higher than most.</p>
-
+		<ul>
+		<li>Sally is not intimidated by technology she only uses her Mac about one hour to two hours a day.</li>
+		<li>She is raising 3 small children and is a caretaker</li>
+		<li>for her disabled veteran husband. Sally stress level is higher than most.</li>
+		</ul>
 		<p><strong>Frustrations and Needs:</strong></p>
+      <ul>
+		<li>Sallie frustration is that she has three small chilren and due to her disable veteran health issues.</li>
+		<li>Sallie needs manage her time more effectively by being able to post quickly.</li>
+		<li>Etsy has solved the issues to sell her product and create a second income stream.</li></ul>
 
-		<p>Sallie frustration is that she has three small chilren and due to her disable veteran health issues.</p>
-		<p>Sallie needs manage her time more effectively by being able to post quickly.</p>
-		<p>Etsy has solved the issues to sell her product and create a second income stream.</p>
-
-		<p><strong>Goals:</strong>Sallie's goal is to provide a creative outlet for people who want to be fashionable while meeting thier fitness goals.</p>
+		<p><strong>Goals:</strong>Sallie's goal is to provide a creative outlet for people who want to be fashionable while meeting their fitness goals.</p>
 
 		<p><strong>Use Story:</strong></p>
-		<p>As a user I want to see if Etsy is a great match for my product as they mostly artisan based.</p>
+		 <p>As a user I want to see if Etsy is a great match for my product as they mostly artisan based.</p>
 
 		<h2><strong>Use Case:</strong></h2>
+
 		<p>Due to her busy home life Sallie will use Etsy to quickly upload her products and also network with other artisans.</p>
 
 		<h2><strong>Interaction</strong></h2>
