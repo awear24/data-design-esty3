d2bb80ab7cd9c3a621bc8c633baaad515b1173a5
Augustina Wear <awear@cnm.edu>
2017-07-17 19:45:42 -0600

    Re did Interaction list and Product list add

--- epic/index.php
+++ epic/index.php
@@ -8,7 +8,7 @@
 		<p><strong>Persona</strong></p>
 		<p><strong>Name:</strong> Sally Seashell</p>
 		<h2><img class="lava-image" src="images/lava.jpg" alt="Sallie"/></h2>
-	<p><strong>Age:</strong> 27</p>
+		<p><strong>Age:</strong> 27</p>
 
 		<p><strong>Profession:</strong></p>
 		<p>Artist specializing in sculpting</p>
@@ -21,15 +21,15 @@
 		</ul>
 		<p><strong>Attitudes and Behaviors:</strong></p>
 		<ul>
-		<li>Sally is not intimidated by technology she only uses her Mac about one hour to two hours a day.</li>
-		<li>She is raising 3 small children and is a caretaker</li>
-		<li>for her disabled veteran husband. Sally stress level is higher than most.</li>
+			<li>Sally is not intimidated by technology she only uses her Mac about one hour to two hours a day.</li>
+			<li>She is raising 3 small children and is a caretaker</li>
+			<li>for her disabled veteran husband. Sally stress level is higher than most.</li>
 		</ul>
 		<p><strong>Frustrations and Needs:</strong></p>
       <ul>
-		<li>Sallie frustration is that she has three small children and due to her disable veteran health issues.</li>
-		<li>Sallie needs manage her time more effectively by being able to post quickly.</li>
-		<li>Etsy has solved the issues to sell her product and create a second income stream.</li>
+			<li>Sallie frustration is that she has three small children and due to her disable veteran health issues.</li>
+			<li>Sallie needs manage her time more effectively by being able to post quickly.</li>
+			<li>Etsy has solved the issues to sell her product and create a second income stream.</li>
 		</ul>
 		<p><strong>Goals:</strong>Sallie's goal is to provide a creative outlet for people who want to be fashionable while meeting
 			their fitness goals.</p>
@@ -43,23 +43,30 @@
 
 		<h2><strong>Interaction</strong></h2>
 		<ul>
-			<li>User needs to list prices for item sizes</li>
-			<li>User needs to list several designs</li>
-			<li>User needs to address custom orders</li>
-			<li>User will list shipping preferences</li>
-			<li>User will have post pictures of inventory</li>
-			<li>User will need to interact with buyer</li>
-		   </ul>
-		     <h2><strong>Conceptual</strong></h2>
-		   <ul>
-				<li>profield(primary key)</li>
-			   <li>profileActivationToken(for account verification</li>
-				<li>profileHandle</li>
-			 	<li>profileEmail<li>
-				<li>profileHash(for account password)</li>
-				<li>profilePhone<li>
-				<li>profileSalt(for account password)</li>
+			<li>User has logged,web page uploads site, user than navigates to inventory listings,web page uploads listing, user opens excel and
+				website auto populates, prices, user enters new prices into cells, web page updates and loads new values.</li>
+			<li>User has logged in, web page acknowledges,user navigates to inventory updates several designs, web page saves and auto populates updated lists.</li>
+			<li>User has logged on, navigates to customer feedback,web page acknowledges and auto populates client list/comments,user navigates to custom orders tab,
+				finds and selects client,sends message,hits send, web page captures/sends.</li>
+			<li>User is logged on to site, navigates the shipping tab,web site acknowledges, auto populates shipping preferences, user picks one,
+				web site launches preference, user fills out drop down menus and hits send, web site and confirms with second verification and send confirmation back to user.</li>
+			<li>User has logged on, web site confirms,user navigates to up load page, inserts jeps,web site deploys them, user updates inventory jeps and confiirms,
+				website acknowledges and post new pictures of inventory on site</li>
+			<li>User logs on, web page acknowledges,sends alert,user reads and reply to buyer, web page then acknowledges and sends will need to buyer</li>
 		</ul>
+		<h2><strong>Conceptual</strong></h2>
+		<h3>Profile</h3>
+		<ul>
+			<li>profield(primary key)</li>
+			<li>profileActivationToken(for account verification</li>
+			<li>profileHandle</li>
+			<li>profileEmail<li>
+			<li>profileHash(for account password)</li>
+			<li>profilePhone<li>
+			<li>profileSalt(for account password)</li>
+		</ul>
+		<h3>Product</h3>
+		<p>User logged on,web acknowledges,user navigates to inventory tab,web page auto populates inventory materials,
+			user updates tab,web site updates of product.</p>
 
-	</body>
-	</html>
+</html>
