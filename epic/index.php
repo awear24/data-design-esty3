0e7ae679776a8964e22928e7dfd309d8f05dfa06
Augustina Wear <awear@cnm.edu>
2017-07-19 10:00:11 -0600

    Finished updating Interaction flow

--- epic/index.php
+++ epic/index.php
@@ -44,24 +44,28 @@
 
 		<h2><strong>Interaction</strong></h2>
 		<ul>
-			<li>User has logged on to Sallie's invensitewe tory listings,web page uploads listing, user opens excel and
-				website auto populates, prices, user enters new prices into cells, web page updates and loads new values.</li>
+			<li>User has logged on to Sallie's site</li>
 			<li>Web pages uploads</li>
 			<li>User clicks on picture of item for purchase they want</li>
-			<li>Web site uploads the picture of braclet and the information and pricing</li>
-			<li>User clicks the drop down menu for their type of fitbit</li>
+			<li>Web site uploads the picture of bracelet and the information and pricing</li>
+			<li>User clicks the drop down menu for their type of Fitbit</li>
 			<li>Web then auto populates a list the various fitbits in drop down menu</li>
-			<li>User then finds her fitbit a Charge 2 in the field and selects it</li>
+			<li>User then finds her Fitbit, a Charge 2 in the field and selects it</li>
 			<li>Web site acknowledged and asks what size</li>
 			<li>User then pick size small</li>
 			<li>Web site acknowledges and ask what design clasp</li>
 			<li>User picks the design they like</li>
-			<li>Web site acknkowledges and auto-populates and ask user to confirm</li>
+			<li>Web site acknowledges and auto-populates and ask user to confirm</li>
 			<li>User confirms</li>
 			<li>Web site navigates to check out menu</li>
 			<li>User then inputs payment information from the drop down menu</li>
 			<li>Web site acknowledges, then asks where does user want receipt sent confirmation</li>
-			<li></li><li></li><li></li><li></li>
+			<li>User tells the site she wants an email</li>
+			<li>Web site acknowledges and then ask what is the zip and to pick a shipping preference</li>
+			<li>User inputs zip and picks shipping preference</li>
+			<li>Website acknowledges and informs user how many days til estimated delivery,asks if user is finished shopping</li>
+			<li>User tells website that they are finished shopping</li>
+			<li>Web site acknowledges and thanks user then updates the inventory stock</li>
 		</ul>
 		<h2><strong>Conceptual</strong></h2>
 		<h3>Profile</h3>
