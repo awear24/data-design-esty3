0ed90207b91f57f0b9c3d5e43d0bb914f2b04f63
Augustina Wear <awear@cnm.edu>
2017-07-20 19:36:18 -0600

    deleted and added SQL

--- epic/sql/.sql
+++ epic/sql/.sql
@@ -1,27 +1,28 @@
- DROP TABLE IF EXISTS item;
- DROP TABLE IF EXISTS profile;
+DROP TABLE IF EXISTS item;
+DROP TABLE IF EXISTS profile;
 
- CREATE TABLE profile (
-	 profileId INT UNSIGNED AUTO_INCREMENT NOT NULL,
-	 profileActivationToken CHAR(32),
-	 profileAtHandle VARCHAR(32) NOT NULL,
-	 profileEmail VARCHAR(128) NOT NULL,
-	 profileHash  CHAR(128) NOT NULL,
-	 profilePhone VARCHAR(32),
-	 profileSalt CHAR(64) NOT NULL,
-	 UNIQUE (profileEmail),
-	 UNIQUE (profileAtHandle),
-	 PRIMARY KEY (profileId)
- );
+CREATE TABLE profile (
+	profileId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+	profileActivationToken CHAR(32),
+	profileAtHandle VARCHAR(32) NOT NULL,
+	profileEmail VARCHAR(128) NOT NULL,
+	profileHash  CHAR(128) NOT NULL,
+	profilePhone VARCHAR(32),
+	profileSalt CHAR(64) NOT NULL,
+	UNIQUE (profileEmail),
+	UNIQUE (profileAtHandle),
+	PRIMARY KEY (profileId)
+);
 
 
 CREATE TABLE item (
-	itemId          INT UNSIGNED AUTO_INCREMENT NOT NULL,
-	itemSize        VARCHAR(140)                NOT NULL,
-	itemDescription VARCHAR(140
-						 )                           NOT NULL,
-	itemPricing     VARCHAR(140)                NOT NULL,
-	INDEX (profileId),
-	FOREIGN KEY (itemIdId) REFERENCES profile (profileId),
-	PRIMARY KEY (itemId)
+	itemId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+	itemProfileId INT UNSIGNED NOT NULL,
+	itemSize VARCHAR(140) NOT NULL,
+	itemDescription VARCHAR(140) NOT NULL,
+	itemPricing VARCHAR(140) NOT NULL,
+	FOREIGN KEY (itemprofileId) REFERENCES profile (profileId),
+	PRIMARY KEY (itemId),
+	UNIQUE (itemPricing),
+	UNIQUE (itemSize)
 );
